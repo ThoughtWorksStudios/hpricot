bf21cb523f95f6b1611b1d750b075c66a5ead30b
Xiao Li <swing1979@gmail.com>
2013-06-06 16:30:34 -0700

    support JRuby 1.7.4

--- ext/fast_xs/FastXsService.java
+++ ext/fast_xs/FastXsService.java
@@ -7,19 +7,19 @@ import java.util.Map;
 import java.util.TreeMap;
 import org.jruby.Ruby;
 import org.jruby.RubyModule;
-import org.jruby.runtime.CallbackFactory;
 import org.jruby.runtime.builtin.IRubyObject;
 import org.jruby.runtime.load.BasicLibraryService;
+import org.jruby.anno.JRubyMethod;
 
 public class FastXsService implements BasicLibraryService {
 
     public boolean basicLoad(final Ruby runtime) throws IOException {
         RubyModule string = runtime.getModule("String");
-        CallbackFactory fact = runtime.callbackFactory(FastXsService.class);
-        string.defineMethod("fast_xs",fact.getFastSingletonMethod("fast_xs"));
+        string.defineAnnotatedMethods(FastXsService.class);
         return true;
     }
 
+    @JRubyMethod(name = "fast_xs")
     public static IRubyObject fast_xs(IRubyObject recv) {
         String string = recv.convertToString().getUnicodeValue();
         StringWriter writer = new StringWriter ((int)(string.length() * 1.5));
